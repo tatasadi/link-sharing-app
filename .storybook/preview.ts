c4eaef6fb44659437a6aad7fa98470cdba334821
Ehsan Tatasadi <ehsan@tatasadi.com>
2024-06-24 12:59:43 +0200

    init project

---
+++ .storybook/preview.ts
@@ -0,0 +1,28 @@
+import '../app/globals.css'
+import type { Preview } from '@storybook/react'
+
+import { withThemeByClassName } from '@storybook/addon-themes'
+
+const preview: Preview = {
+	parameters: {
+		controls: {
+			matchers: {
+				color: /(background|color)$/i,
+				date: /Date$/i,
+			},
+		},
+	},
+
+	decorators: [
+		withThemeByClassName({
+			themes: {
+				// nameOfTheme: 'classNameForTheme',
+				light: '',
+				dark: 'dark',
+			},
+			defaultTheme: 'light',
+		}),
+	],
+}
+
+export default preview
