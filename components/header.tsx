5900588f94786d622ee392c9b57586278749e3c4
Ehsan Tatasadi <ehsan@tatasadi.com>
2024-10-18 13:57:29 +0200

    fix some header styles in mobile

--- components/header.tsx
+++ components/header.tsx
@@ -14,20 +14,25 @@ import LogoutButton from './logout-button'
 export default function Header({ className = '' }: { className?: string }) {
 	const pathname = usePathname()
 	return (
-		<header className={cn('flex bg-white p-4 pl-6 sm:rounded-xl sm:m-6', className)}>
+		<header
+			className={cn(
+				'flex bg-white p-4 pl-6 sm:rounded-xl sm:m-6 justify-between items-center',
+				className,
+			)}
+		>
 			<Logo />
-			<Link href="/links" className="ml-auto">
+			<Link href="/links" className="sm:ml-auto">
 				<ButtonWithIcon asChild icon={<IconLink />} isActive={pathname === '/links'}>
 					Links
 				</ButtonWithIcon>
 			</Link>
-			<Link href="/profile" className="mr-auto">
+			<Link href="/profile" className="sm:mr-auto">
 				<ButtonWithIcon asChild icon={<IconProfile />} isActive={pathname === '/profile'}>
 					Profile Details
 				</ButtonWithIcon>
 			</Link>
 			<Link href="/preview">
-				<Button variant="secondary">
+				<Button variant="secondary" className="px-3 py-2 sm:px-7 sm:py-3">
 					<span className="sm:hidden">
 						<IconEye />
 					</span>
