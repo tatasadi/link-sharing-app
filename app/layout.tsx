ab1915f5ae79c595e01b520d93f7e9e86e1dba7a
Ehsan Tatasadi <ehsan@tatasadi.com>
2024-10-18 18:21:25 +0200

    better description

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,7 +9,7 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata: Metadata = {
 	title: 'Link Sharing App',
 	description:
-		'A full-stack link-sharing app built with React, TypeScript, Next.js, and Tailwind CSS. Users can manage personalized profile links, upload profile images to Amazon S3, and authenticate securely using NextAuth.',
+		'An intuitive platform for developers to create personalized profiles that consolidate links to all their social media accounts, making it easy to share their online presence with others.',
 }
 
 export default async function RootLayout({
