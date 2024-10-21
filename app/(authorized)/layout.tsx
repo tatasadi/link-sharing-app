1d876f966b3f09e7f161c5e2280bb958c2901f64
Ehsan Tatasadi <ehsan@tatasadi.com>
2024-10-21 17:27:47 +0200

    fix typo

--- app/(authorized)/layout.tsx
+++ app/(authorized)/layout.tsx
@@ -1,6 +1,6 @@
 import UpdateUserInStore from '@/components/update-user-in-store'
 
-export default async function AutohrizedLayout({ children }: { children: React.ReactNode }) {
+export default async function AuthorizedLayout({ children }: { children: React.ReactNode }) {
 	return (
 		<>
 			{children}
